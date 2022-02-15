62afac976a6038b78c8981725454edf637876185
Emerson <99724447+Emerson-Pereira-programando@users.noreply.github.com>
2022-02-15 16:45:15 -0300

    Update CalculionJS.js

--- CalculionJS.js
+++ CalculionJS.js
@@ -1,23 +1,19 @@
 var valor;
 var enviar = document.querySelector("Button");
 var h3 = document.getElementById("h3for").innerHTML;
+var z = 10;
 enviar.onclick = function() {
     valor = document.querySelector("#valor");
     var k = Number.parseInt(valor.value)
     var valor01 = k * 1;
-    for (var i = 1; i <= 10; i++) {
+    var c = z * k
+    for (var i = 1; i <= 9; i++) {
         valor01 = k * i;
         h3 = h3 + "<h3>" + k + " x  " + i + " = " + valor01 + "</h3>";
         document.getElementById("h3for").innerHTML = h3;
+        if (i == 9) {
+            h3 = h3 + "<h3>" + k + " x  " + z + " = " + c + "——————————————" + "</h3>";
+            document.getElementById("h3for").innerHTML = h3;
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
